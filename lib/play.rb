4069575b68ef27274eb942c773f5f8d01a200827
Jared Shelby <jared.shelby@yale.edu>
2022-05-18 00:42:54 -0400

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,10 @@ def turn(board)
 end
 
 # Define your play method below
+def play(board)
+  num_turns_completed = 0
+  while num_turns_completed < 9 do
+    turn(board)
+    num_turns_completed += 1
+  end
+end
